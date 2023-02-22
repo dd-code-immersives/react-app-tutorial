cc367baa106a50a22ce7b9c5cf2aa980c7e2b7c4
dd-code-immersives <domilamad89@gmail.com>
2023-02-22 15:10:37 -0500

    added life cycle methods to Clock Component

--- src/Clock.js
+++ src/Clock.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
  class Clock extends Component {
 
+    // 1.
     // add a constructor
     // add local state (inside a component )
     // this is called when the Clock Component is created
@@ -9,7 +10,25 @@ import React, { Component } from 'react';
         super(props);
         this.state = {date: new Date()};
     }
+    // 3.
+    // runs after the component output has been rendered to the DOM.
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick()
+        );
+    }
+    // 4. 
+    // happens after  when component is destroyed
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
 
+    // local helper function to get time
+    tick() {
+        this.setState({ date: new Date()});
+    }
+
+    // 2. The Component is rendered
     render() {
         return (
             <div>
